7dfc2c3b1c3b603333cfc0bb57d7cf149337bc77
Nikos <nikolonikakos03@gmail.com>
2025-02-12 18:46:35 +0200

    LECTURE #1 BASIC UNDERSTANDING C++

---
+++ LEC1_1.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+//using str = string;
+
+int main() {
+    cout << "Please enter your first name (followed " << "by 'enter'):\n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hello, " << first_name << "\n";
+}
---
+++ LEC1_3.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() {
+    cout << "please enter your first name and age: ";
+    string first_name;
+    int age;
+    cin >> first_name >> age;
+    cout << "Hello, " << first_name << " age: " << age << '\n';
+}
